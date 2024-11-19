c2b99e46b7271333fc2bb03785ee93d58670603d
Kellan Cerveny <kjcerveny@gmail.com>
2024-11-19 09:47:19 -0600

    Max window, better cursor, zoomable
    Wow. This did not used to be this easy!

--- GPT_open_tif.py
+++ GPT_open_tif.py
@@ -8,6 +8,7 @@ Created on Tue Nov 19 08:55:03 2024
 import rasterio
 from rasterio.plot import show
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Cursor
 from shapely.geometry import Point
 import sys
 
@@ -19,7 +20,8 @@ waypoints = []
 
 def onclick(event):
     """Handle mouse click events to add waypoints."""
-    if event.inaxes:  # Ensure the click is within the plot
+    # Check if the toolbar is inactive (not in zoom or pan mode)
+    if plt.get_current_fig_manager().toolbar.mode == '' and event.inaxes:  # Ensure the click is within the plot
         # Get pixel coordinates from the click
         x_pixel, y_pixel = int(event.xdata), int(event.ydata)
         
@@ -46,13 +48,26 @@ with rasterio.open(geo_tiff_path) as dataset:
     transform = dataset.transform
 
     # Plot the GeoTIFF
-    fig, ax = plt.subplots()
-    show(dataset, ax=ax)
-    plt.title("Click to add waypoints")
+    fig, ax = plt.subplots(figsize=(10, 10))
+    show(dataset, ax=ax, title="Click to Add Waypoints")
+
+    # Add a crosshair cursor for precision
+    cursor = Cursor(ax, useblit=True, color='green', linewidth=1)
+
+     # Maximize the plot window but keep it windowed
+    mng = plt.get_current_fig_manager()
+    try:
+        mng.window.state('zoomed')  # Works for TkAgg backend (maximize)
+    except AttributeError:
+        mng.resize(*mng.window.maxsize())  # For other backends, resize to screen size
+
 
     # Connect the onclick event to the plot
     fig.canvas.mpl_connect('button_press_event', onclick)
     plt.show()
+
+
+
 
 # Print all waypoints after the interaction
 print("Waypoints (georeferenced):")
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Open a GeoTIFF image, click to add mission waypoints, then export your plan.
 
 ## TODO
 - [ ] Input GeoTIFFs exceed GitHub 100MB input file size limit. Might need LFS.
-- [ ] Zoom/enhance visual size of tif
+- [x] Zoom/enhance visual size of tif
 - [ ] Draw lines between waypoints
 - [ ] Number the waypoints
 - [ ] Export plan to a file
